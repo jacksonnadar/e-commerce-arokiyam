7cf7f3429d9dc64f42bc1982e837359c524f34d9
jackson <jackson@pop-os.localdomain>
2020-12-07 21:12:42 +0530

    screen size

--- src/assets/components/Hero/Hero.tsx
+++ src/assets/components/Hero/Hero.tsx
@@ -13,30 +13,33 @@ export interface Props {}
 const Hero: React.FC<Props> = () => {
   return (
     <div className="Hero">
-      <Carosuel>
-        <img src={carosuel} alt="" />
-        <img src={carosuel} alt="" />
-        <img src={carosuel} alt="" />
-        <img src={carosuel} alt="" />
-      </Carosuel>
-      <section className="intro">
-        <h1 className="arokiyam">AROKIYAM</h1>
-        <p className="subtitle">Eat right, be bright.</p>
-        <p className="title">your health our concern.</p>
-        <div className="buttons">
-          <div className="call-us">
-            <button>Call Us&nbsp;&nbsp;&nbsp;</button>
+      <div className="hero-wraper">
+        <Carosuel>
+          <img src={carosuel} alt="" />
+          <img src={carosuel} alt="" />
+          <img src={carosuel} alt="" />
+          <img src={carosuel} alt="" />
+        </Carosuel>
+        <section className="intro">
+          <h1 className="arokiyam">AROKIYAM</h1>
+          <p className="subtitle">Eat right, be bright.</p>
+          <p className="title">your health our</p>
+          <p className="title">concern.</p>
+          <div className="buttons">
+            <div className="call-us">
+              <button>Call Us&nbsp;&nbsp;&nbsp;</button>
+            </div>
+            <div className="shop-now">
+              <button>Shop Now</button>
+            </div>
           </div>
-          <div className="shop-now">
-            <button>Shop Now</button>
-          </div>
-        </div>
-        <img className="machine" src={machine} alt="oil machine" />
-      </section>
-      <img src={bottels} alt="bottels" className="bottels floating-images" />
-      <img src={peanut} alt="peanut" className="peanut floating-images" />
-      <img src={decorDots} alt="" className="decor-dots floating-images" />
-      <img src={down} alt="" className="down floating-images" />
+          <img className="machine" src={machine} alt="oil machine" />
+        </section>
+        <img src={bottels} alt="bottels" className="bottels floating-images" />
+        <img src={peanut} alt="peanut" className="peanut floating-images" />
+        <img src={decorDots} alt="" className="decor-dots floating-images" />
+        <img src={down} alt="" className="down floating-images" />
+      </div>
     </div>
   );
 };
